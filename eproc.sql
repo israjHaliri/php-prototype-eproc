06cbefbce08fb667c3b212731d30c4ae5150b6b4
israjHaliri <israj.haliri@gmail.com>
2015-09-20 13:37:22 +0700

    fixing database

--- eproc.sql
+++ eproc.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
+-- version 4.4.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 28, 2015 at 10:03 AM
+-- Generation Time: Sep 20, 2015 at 07:36 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.3.29
 
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `eproc`
@@ -33,14 +33,17 @@ CREATE TABLE IF NOT EXISTS `berita` (
   `date_publish_berita` varchar(25) NOT NULL,
   `time_publish_berita` time NOT NULL,
   `image_berita` varchar(500) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=214 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=217 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `berita`
 --
 
 INSERT INTO `berita` (`id_berita`, `title_berita`, `description_berita`, `date_publish_berita`, `time_publish_berita`, `image_berita`) VALUES
-(213, 'berita 12', '', '2015-06-13', '01:00:00', 'berita 122015-06-13admin-lib1.png');
+(213, 'berita 1', '<p>berita 1</p>\r\n', '2015-06-13', '01:00:00', 'berita 12015-06-13Banner-3-980x360.jpg'),
+(214, 'berita 2', '<p>berita 2</p>\r\n', '2015-09-02', '01:00:00', 'berita 22015-09-02Banner-3-980x360.jpg'),
+(215, 'berita 3', '<p>berita 3</p>\r\n', '2015-09-03', '01:00:00', 'berita 32015-09-03Banner-3-980x360.jpg'),
+(216, 'berita 4', '<p>berita 4</p>\r\n', '', '01:00:00', 'berita 4Banner-3-980x360.jpg');
 
 -- --------------------------------------------------------
 
@@ -55,22 +58,6 @@ CREATE TABLE IF NOT EXISTS `contact_us` (
   `subject` varchar(100) NOT NULL,
   `pesan` text NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `contact_us`
---
-
-INSERT INTO `contact_us` (`id_contact`, `nama`, `email`, `subject`, `pesan`) VALUES
-(2, 'q', 'q', 'q', 'q'),
-(3, 'q', 'q', 'q', 'q'),
-(4, 'vasva', '2015-05-08', 'dsasdv', ''),
-(5, 'ASD', '', 'qwd', ''),
-(6, 'ASDA', 'asd2afg@sdfa', 'bisnis', 'ASD'),
-(7, 'asd', 'asd2afg@sdfa', 'bisnis', 'ASd'),
-(8, 'WQDQWJIRJIR', 'QWD@SADF', 'komplen', 'WDQ'),
-(9, 'SETTAN', 'SETTA@FAG', 'komplen', 'SETTAN'),
-(10, 'sistim', 'ssistim@gmail', 'komplen', 'aduh cape'),
-(11, '181', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -89,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `eproc` (
 --
 
 INSERT INTO `eproc` (`title`, `description`, `id_eproc`) VALUES
-('eProcurement', '<p>This prototype eProcurement&nbsp;PLN aplication</p>\r\n\r\n<p>This prototype eProcurement&nbsp;PLN aplication</p>\r\n\r\n<p>This prototype eProcurement&nbsp;PLN aplication</p>\r\n', 3);
+('eProcurement', '<p>This web containt of</p>\r\n\r\n<p>prototype eProcurement&nbsp;PLN aplication</p>\r\n\r\n<p>copyright 2015</p>\r\n', 3);
 
 -- --------------------------------------------------------
 
@@ -107,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `faqs` (
 --
 
 INSERT INTO `faqs` (`description`, `id_faqs`) VALUES
-('<p>registrasi terlebih dahulu dengan mengirimkan data profile perusahaan dan kelengkapan bukti dokumen sesuai dengan bidang usahanya</p>\r\n\r\n<p>registrasi terlebih dahulu dengan mengirimkan data profile perusahaan dan kelengkapan bukti dokumen sesuai dengan bidang usahanya</p>\r\n\r\n<p>registrasi terlebih dahulu dengan mengirimkan data profile perusahaan dan kelengkapan bukti dokumen sesuai dengan bidang usahanya</p>\r\n\r\n<p>registrasi terlebih dahulu dengan mengirimkan data profile perusahaan dan kelengkapan bukti dokumen sesuai dengan bidang usahanya sebagai berikut</p>\r\n\r\n<p>Pada saat tertentu pln belum memerlukan komoditi tertentu, sehingga calon rekanan dengan bidang usaha yang bersangkutan belum dipertimbangkan untuk menjadi rekanan</p>\r\n\r\n<p>Jumlah rekanan yang terdaftar di pln pada saat tertentu sudah mencukupi untuk memenuhi kebutuhan pln Persyaratan untuk menjadi rekanan pln belum dapat dipenuhi atau oleh calon rekanan Tidak sesuai dengan kebijakan pengadaan Pernah dan atau sedang ada permasalahan yang menyangkut kinerja yang tidak memuaskan dengan pengadaan pln sebelumnya Berdasarkan penilaian (assessment) atau referensi, bahwa calon rekanan yang bersangkutan mempunyai kinerja yang tidak baik atau masih perlu dipertimbangkan untuk dapat menjadi rekanan.</p>\r\n\r\n<p>Pada saat tertentu pln belum memerlukan komoditi tertentu, sehingga calon rekanan dengan bidang usaha yang bersangkutan belum dipertimbangkan untuk menjadi rekanan Jumlah rekanan yang terdaftar di pln pada saat tertentu sudah mencukupi untuk memenuhi kebutuhan pln Persyaratan untuk menjadi rekanan pln belum dapat dipenuhi atau oleh calon rekanan Tidak sesuai dengan kebijakan pengadaan Pernah dan atau sedang ada permasalahan yang menyangkut kinerja yang tidak memuaskan dengan pengadaan pln sebelumnya Berdasarkan penilaian (assessment) atau referensi, bahwa calon rekanan yang bersangkutan mempunyai kinerja yang tidak baik atau masih perlu dipertimbangkan untuk dapat menjadi rekanan.</p>\r\n\r\n<p>registrasi terlebih dahulu dengan mengirimkan data profile perusahaan dan kelengkapan bukti dokumen sesuai dengan bidang usahanya</p>\r\n\r\n<p>registrasi terlebih dahulu dengan mengirimkan data profile perusahaan dan kelengkapan bukti dokumen sesuai dengan bidang usahanya sebagai berikut</p>\r\n\r\n<p>Pada saat tertentu pln belum memerlukan komoditi tertentu, sehingga calon rekanan dengan bidang usaha yang bersangkutan belum dipertimbangkan untuk menjadi rekanan</p>\r\n\r\n<p>Jumlah rekanan yang terdaftar di pln pada saat tertentu sudah mencukupi untuk memenuhi kebutuhan pln Persyaratan untuk menjadi rekanan pln belum dapat dipenuhi atau oleh calon rekanan Tidak sesuai dengan kebijakan pengadaan Pernah dan atau sedang ada permasalahan yang menyangkut kinerja yang tidak memuaskan dengan pengadaan pln sebelumnya Berdasarkan penilaian (assessment) atau referensi, bahwa calon rekanan yang bersangkutan mempunyai kinerja yang tidak baik atau masih perlu dipertimbangkan untuk dapat menjadi rekanan. Pada saat tertentu pln belum memerlukan komoditi tertentu, sehingga calon rekanan dengan bidang usaha yang bersangkutan belum dipertimbangkan untuk menjadi rekanan Jumlah rekanan yang terdaftar di pln pada saat tertentu sudah mencukupi untuk memenuhi kebutuhan pln Persyaratan untuk menjadi rekanan pln belum dapat dipenuhi atau oleh calon rekanan Tidak sesuai dengan kebijakan pengadaan Pernah dan atau sedang ada permasalahan yang menyangkut kinerja yang tidak memuaskan dengan pengadaan pln sebelumnya Berdasarkan penilaian (assessment) atau referensi, bahwa calon rekanan yang bersangkutan mempunyai kinerja yang tidak baik atau masih perlu dipertimbangkan untuk dapat menjadi rekanan.</p>\r\n\r\n<p>&nbsp;</p>\r\n', 1),
+('<p>registrasi terlebih dahulu dengan mengirimkan data profile perusahaan dan kelengkapan bukti dokumen sesuai dengan bidang usahanya</p>\r\n\r\n<p>Pada saat tertentu pln belum memerlukan komoditi tertentu, sehingga calon rekanan dengan bidang usaha yang bersangkutan belum dipertimbangkan untuk menjadi rekanan</p>\r\n\r\n<p>Jumlah rekanan yang terdaftar di pln pada saat tertentu sudah mencukupi untuk memenuhi kebutuhan pln Persyaratan untuk menjadi rekanan pln belum dapat dipenuhi atau oleh calon rekanan Tidak sesuai dengan kebijakan pengadaan Pernah dan atau sedang ada permasalahan yang menyangkut kinerja yang tidak memuaskan dengan pengadaan pln sebelumnya Berdasarkan penilaian (assessment) atau referensi, bahwa calon rekanan yang bersangkutan mempunyai kinerja yang tidak baik atau masih perlu dipertimbangkan untuk dapat menjadi rekanan.</p>\r\n\r\n<p>&nbsp;</p>\r\n', 1),
 ('', 2);
 
 -- --------------------------------------------------------
@@ -123,14 +110,16 @@ CREATE TABLE IF NOT EXISTS `lelang` (
   `date_publish_lelang` varchar(50) NOT NULL,
   `time_publish_lelang` time NOT NULL,
   `image_lelang` varchar(500) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lelang`
 --
 
 INSERT INTO `lelang` (`id_lelang`, `title_lelang`, `description_lelang`, `date_publish_lelang`, `time_publish_lelang`, `image_lelang`) VALUES
-(10, 'lelang 12', '', '2015-06-05', '01:00:00', 'lelang 122015-06-05front-lib2.png');
+(11, 'lelang 1', '<p>lelang 1</p>\r\n', '2015-09-01', '01:00:00', 'lelang 12015-09-01Banner-3-980x360.jpg'),
+(12, 'lelang 2', '<p>lelang 2</p>\r\n', '2015-09-10', '01:00:00', 'lelang 22015-09-10Banner-3-980x360.jpg'),
+(13, 'lelang 4', '<p>lelang 4</p>\r\n', '2015-09-04', '01:00:00', 'lelang 42015-09-04Banner-3-980x360.jpg');
 
 -- --------------------------------------------------------
 
@@ -146,16 +135,6 @@ CREATE TABLE IF NOT EXISTS `pakta_integritas` (
   `date_publish` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `pakta_integritas`
---
-
-INSERT INTO `pakta_integritas` (`name_file`, `type_file`, `file_size`, `file`, `date_publish`) VALUES
-('DVSD', 'rar', '125247', '../assets/files/DVSD.rar', '2015-05-24'),
-('DFSDF', 'rar', '125247', '../assets/files/DFSDF.rar', '2015-05-24'),
-('babin', 'rar', '1416', '../assets/files/babin.rar', '2015-05-24'),
-('TEST', 'rar', '125247', '../assets/files/TEST.rar', '2015-05-23');
-
 -- --------------------------------------------------------
 
 --
@@ -166,15 +145,16 @@ CREATE TABLE IF NOT EXISTS `pemenang` (
   `id_pemenang` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `pengadaan_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pemenang`
 --
 
 INSERT INTO `pemenang` (`id_pemenang`, `user_id`, `pengadaan_id`) VALUES
-(3, 15, 25),
-(5, 15, 25);
+(6, 15, 27),
+(7, 15, 27),
+(8, 15, 28);
 
 -- --------------------------------------------------------
 
@@ -189,15 +169,16 @@ CREATE TABLE IF NOT EXISTS `pengadaan` (
   `date_publish_pengadaan` varchar(50) NOT NULL,
   `time_publish_pengadaan` time NOT NULL,
   `image_pengadaan` varchar(500) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pengadaan`
 --
 
 INSERT INTO `pengadaan` (`id_pengadaan`, `title_pengadaan`, `description_pengadaan`, `date_publish_pengadaan`, `time_publish_pengadaan`, `image_pengadaan`) VALUES
-(25, 'testing23', '<p>ad</p>\r\n', '2015-06-06', '01:00:00', 'testing232015-06-06admin-lib2.png'),
-(26, 'pengadaan 2', '', '', '01:00:00', 'pengadaan 2cs.png');
+(27, 'pengadaan 1', '<p>pengadaan 1</p>\r\n', '2015-09-02', '01:00:00', 'pengadaan 12015-09-02Banner-3-980x360.jpg'),
+(28, 'pengadaan 2', '<p>pengadaan 2</p>\r\n', '2015-09-03', '01:00:00', 'pengadaan 22015-09-03Banner-3-980x360.jpg'),
+(29, 'pengadaan  4', '<p>pengadaan &nbsp;4</p>\r\n', '', '01:00:00', 'pengadaan  4Banner-3-980x360.jpg');
 
 -- --------------------------------------------------------
 
@@ -222,12 +203,8 @@ CREATE TABLE IF NOT EXISTS `supplier` (
 --
 
 INSERT INTO `supplier` (`id_supplier`, `nama`, `alamat`, `password`, `nama_perusahaan_penyedia`, `no_pkp`, `bentuk_usaha`, `npwp`, `status`) VALUES
-(2, 'sada', '1saFa', 'ZmFm', '1dsfa', '1awefa', '1ewf', '1ewaf', 'non-aktif'),
-(3, 'pln', 'pln', 'cGxu', 'pln', 'pln', 'pln', 'pln', 'non-aktif'),
-(4, 'pln', 'pln', 'cGxu', 'pln', 'pln', 'pln', 'pln', 'aktif'),
-(5, 'israj', 'asd', 'MDI2', 'asd', 'asd', 'asd', 'asd', 'aktif'),
-(6, 'pln', 'pln', 'cGxu', 'pln', 'pln', 'pln', 'pln', 'aktif'),
-(7, 'ihin', 'ihin', 'aWhpbg==', 'ihin', 'ihin', 'ihin', 'ihin', 'non-aktif');
+(2, 'ipp', 'ipp', 'aXBw', 'ipp', 'ipp', 'ipp', 'ipp', 'aktif'),
+(3, 'pln', 'pln', 'cGxu', 'pln', 'pln', 'pln', 'pln', 'non-aktif');
 
 -- --------------------------------------------------------
 
@@ -249,7 +226,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `email`, `level`, `image`) VALUES
-(15, 'israj', 'MDI2', 'israj.haliri@gmail.com', 'admin', 'israjisraj.haliri@gmail.comisrajisraj.haliri@gmail.com165138_173677932673237_4498017_n.jpg');
+(15, 'israj', 'MDI2', 'israj.haliri@gmail.com', 'admin', 'israjisraj.haliri@gmail.comme.jpg');
 
 --
 -- Indexes for dumped tables
@@ -321,7 +298,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `berita`
 --
 ALTER TABLE `berita`
-  MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=214;
+  MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=217;
 --
 -- AUTO_INCREMENT for table `contact_us`
 --
@@ -341,17 +318,17 @@ ALTER TABLE `faqs`
 -- AUTO_INCREMENT for table `lelang`
 --
 ALTER TABLE `lelang`
-  MODIFY `id_lelang` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+  MODIFY `id_lelang` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `pemenang`
 --
 ALTER TABLE `pemenang`
-  MODIFY `id_pemenang` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id_pemenang` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `pengadaan`
 --
 ALTER TABLE `pengadaan`
-  MODIFY `id_pengadaan` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
+  MODIFY `id_pengadaan` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `supplier`
 --
